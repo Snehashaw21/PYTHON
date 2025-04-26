c2ed004f6a20b7bcb4c0a2873c5ee887b449f045
Snehashaw21 <snehashaw261@gmail.com>
2025-04-26 19:45:33 +0530

    Create Calculate_the_area_of_triangle.py

---
+++ AREA/Calculate_the_area_of_triangle.py
@@ -0,0 +1,7 @@
+#Calculate the area of a triangle
+
+
+height=float(input("Height of a triangle: "))
+base=float(input("Base of a triangle: "))
+area=(1/2)*height*base
+print("Area of a triangle=",area)
