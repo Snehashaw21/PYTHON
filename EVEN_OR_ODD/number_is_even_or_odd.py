26725a2a9a0cade87383f3c2deb0ecbf06446e25
Snehashaw21 <snehashaw261@gmail.com>
2025-04-28 21:50:24 +0530

    number_is_even_or_odd.py

---
+++ EVEN_OR_ODD/number_is_even_or_odd.py
@@ -0,0 +1,9 @@
+#Check if the number is even or odd
+#Number is given by user(user input)
+
+
+a=int(input("Enter your number:"))
+if a%2==0 :
+    print("It is an even number.")
+else:
+    print("It is an odd number.")
