b82cec19590b907a5af260e66d9a45f663b0995b
Snehashaw21 <snehashaw261@gmail.com>
2025-05-01 14:24:53 +0530

    check_number_is_prime_or_not.py

---
+++ PRIME_NUMBER/check_number_is_prime_or_not.py
@@ -0,0 +1,13 @@
+#Check the given number is prime number or not
+
+num = int(input("Enter the number: "))
+
+if num == 1:
+    print("It is not a prime number.")
+elif num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("It is not a prime number.")
+            break
+    else:
+        print("It is a prime number.")
