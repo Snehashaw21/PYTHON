759765f755c760eb9ff69a813a09c253e12286ee
Snehashaw21 <snehashaw261@gmail.com>
2025-05-17 22:13:36 +0530

    Create dictionaries.py

---
+++ DICTIONARIES/dictionaries.py
@@ -0,0 +1,8 @@
+#Print and update the marks that is given below
+
+mark={"Ram":99,"Sita":96,"raju":99}
+print(mark.items())
+print(mark.keys())
+mark.update({"Ram":100, "Ram2":65})
+print(mark)
+print(mark.get("Ram"))
