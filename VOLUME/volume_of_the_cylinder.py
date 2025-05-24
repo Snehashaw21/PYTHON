8c93f266623dcc2d5736ac4151ec425436faa7e1
Snehashaw21 <snehashaw261@gmail.com>
2025-05-24 20:58:14 +0530

    Create volume_of_the_cylinder.py

---
+++ VOLUME/volume_of_the_cylinder.py
@@ -0,0 +1,10 @@
+#calculates the volume of a cylinder
+
+import math
+
+r = float(input("Enter radius: "))
+h = float(input("Enter height: "))
+
+volume = math.pi * r * r * h
+
+print("Volume of the cylinder is:", volume)
