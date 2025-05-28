422859274ea0e7ec1c1f8bac7465e2cfb3cc22e3
Snehashaw21 <snehashaw261@gmail.com>
2025-05-28 20:06:22 +0530

    volume_of_the_pyramid.py

---
+++ VOLUME/volume_of_the_pyramid.py
@@ -0,0 +1,9 @@
+#Calculate the volume of the pyramid
+
+length = float(input("Enter length of the base: "))
+width = float(input("Enter width of the base: "))
+height = float(input("Enter height of the pyramid: "))
+
+volume = (length * width * height) / 3
+
+print("Volume of the pyramid is:", volume)
