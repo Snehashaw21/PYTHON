b861bd112ba72c21e45a6bdf7ccdb53ccaae4dcb
Snehashaw21 <snehashaw261@gmail.com>
2025-05-29 19:29:52 +0530

    volume_of_torus.py

---
+++ VOLUME/volume_of_torus.py
@@ -0,0 +1,10 @@
+#Calculate volume of torus 
+
+import math
+
+R = float(input("Enter the major radius (R): "))
+r = float(input("Enter the minor radius (r): "))
+
+volume = 2 * math.pi**2 * R * r**2
+
+print("Volume of the torus is:", volume)
