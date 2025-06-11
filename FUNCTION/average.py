fee2ec7674e9578b07670470394ef9b82745bcd5
Snehashaw21 <snehashaw261@gmail.com>
2025-06-11 11:58:04 +0530

    Create average.py

---
+++ FUNCTION/average.py
@@ -0,0 +1,13 @@
+#Calculate average of three number using functions
+#This program find two times average of three numbers. 
+
+def avg():
+    a=int(input("Enter the number: "))
+    b=int(input("Enter the number: "))
+    c=int(input("Enter the number: "))
+
+    average=(a+b+c)/3
+    print(average)
+
+avg()
+avg()
