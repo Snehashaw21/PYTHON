fb7f1cfeabeb19ecad2a59d6fe781ab3285653fc
Snehashaw21 <snehashaw261@gmail.com>
2025-07-01 22:05:38 +0530

    encapsulation.py

---
+++ FUNCTION/encapsulation.py
@@ -0,0 +1,15 @@
+#ENCAPSULATION:
+
+class Account:
+    def __init__(self, balance):
+        self.__balance = balance  
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+acc = Account(1000)
+acc.deposit(500)
+print(acc.get_balance()) 
