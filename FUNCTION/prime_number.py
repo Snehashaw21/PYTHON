946341d3ee4e1e92966a39addd06c48c8fc8ce30
Snehashaw21 <snehashaw261@gmail.com>
2025-07-05 22:06:14 +0530

    prime_number.py

---
+++ FUNCTION/prime_number.py
@@ -0,0 +1,16 @@
+#check the prime number in between 1-50:
+#2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+# Check and print prime numbers between 1 and 50
+print("Prime numbers between 1 and 50 are:")
+for num in range(1, 51):
+    if is_prime(num):
+        print(num, end=' ')
