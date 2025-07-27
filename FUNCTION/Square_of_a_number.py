2d2ec7bb1d099a55b5dedd63d279973e8a6d0ee1
Snehashaw21 <snehashaw261@gmail.com>
2025-07-27 22:29:11 +0530

    square_of_a_number.py

---
+++ FUNCTION/Square_of_a_number.py
@@ -0,0 +1,11 @@
+# Square of a Number using Function
+# Function to calculate square:
+
+
+def find_square(number):
+    return number * number
+
+num = int(input("Enter a number: "))
+
+result = find_square(num)
+print("The square of", num, "is", result)
