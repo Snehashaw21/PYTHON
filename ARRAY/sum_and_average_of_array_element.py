2e9d7e197c9351c308796ec1eaf6d0c93cac722b
Snehashaw21 <snehashaw261@gmail.com>
2025-07-31 15:05:28 +0530

    sum_and_average_of_array_element.py

---
+++ ARRAY/sum_and_average_of_array_element.py
@@ -0,0 +1,12 @@
+#Calculate the sum and average of array elements:
+
+arr = [10, 20, 30, 40, 50]
+
+total = 0
+for num in arr:
+    total += num
+
+average = total / len(arr)
+
+print("Sum of array elements:", total)
+print("Average of array elements:", average)
