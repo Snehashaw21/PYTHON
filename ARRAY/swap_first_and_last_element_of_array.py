377f6fd5b6392b1763f2557ab3cc828910f4c9f3
Snehashaw21 <snehashaw261@gmail.com>
2025-08-25 10:57:14 +0530

    swap_first_and_last_element_of_array.py

---
+++ ARRAY/swap_first_and_last_element_of_array.py
@@ -0,0 +1,10 @@
+#Swap the first and last element of an array:
+
+def swap_first_last(arr):
+    if len(arr) > 1:   # only swap if at least 2 elements
+        arr[0], arr[-1] = arr[-1], arr[0]
+    return arr
+
+arr = [10, 20, 30, 40, 50]
+print("Original array:", arr)
+print("After swapping:", swap_first_last(arr))
