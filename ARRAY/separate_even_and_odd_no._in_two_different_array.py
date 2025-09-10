f2257e9a24eea2199b6c4e67f35063ded13a4c8d
Snehashaw21 <snehashaw261@gmail.com>
2025-09-10 21:17:14 +0530

    separate_even_and_odd_no._in_two_different_array.py

---
+++ ARRAY/separate_even_and_odd_no._in_two_different_array.py
@@ -0,0 +1,18 @@
+#Separate even and odd numbers into two different arrays:
+
+def separate_even_odd(arr):
+    even = []
+    odd = []
+    for num in arr:
+        if num % 2 == 0:
+            even.append(num)
+        else:
+            odd.append(num)
+    return even, odd
+
+arr = [10, 21, 4, 45, 66, 93, 22]
+even, odd = separate_even_odd(arr)
+
+print("Original array:", arr)
+print("Even numbers:", even)
+print("Odd numbers:", odd)
