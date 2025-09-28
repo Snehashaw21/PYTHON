0e3ac41c0d699c62630b80457efaf085a5d20b34
Snehashaw21 <snehashaw261@gmail.com>
2025-09-28 10:19:12 +0530

    majority_of_element.py

---
+++ ARRAY/majority_of_element.py
@@ -0,0 +1,14 @@
+#Find the majority element (element that appears more than n/2 times):
+
+def element(arr):
+    n = len(arr)
+    freq = {}
+
+    for num in arr:
+        freq[num] = freq.get(num, 0) + 1
+        if freq[num] > n // 2:
+            return num   
+    return None  
+
+arr = [3, 3, 4, 2, 3, 3, 5, 3]
+print("Majority Element:", element(arr)) 
