764ccdffcb39269a48c3526928cc3b2d5b755f81
Snehashaw21 <snehashaw261@gmail.com>
2025-10-03 08:15:29 +0530

    calculate_area_of_a_square.py

---
+++ AREA/calculate_area_of_a_square.py
@@ -0,0 +1,6 @@
+# Calculate area of a square:
+
+side = float(input("Enter the length of the side of the square: "))
+# formula for area of square = side × side
+area = side * side
+print("The area of the square is:", area)
