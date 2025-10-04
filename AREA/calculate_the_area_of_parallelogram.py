c60922ec847a8df4bb3304c1a481e24bc09f39b2
Snehashaw21 <snehashaw261@gmail.com>
2025-10-04 19:26:31 +0530

    calculate_the_area_of_parallelogram.py

---
+++ AREA/calculate_the_area_of_parallelogram.py
@@ -0,0 +1,7 @@
+# Calculate area of a parallelogram:
+
+base = float(input("Enter the base of the parallelogram: "))
+height = float(input("Enter the height of the parallelogram: "))
+
+area = base * height
+print("The area of the parallelogram is:", area)
