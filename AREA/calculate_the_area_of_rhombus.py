3a2ed2545ea898e2164d1b29d4395480ec1aa594
Snehashaw21 <snehashaw261@gmail.com>
2025-10-06 11:37:45 +0530

    calculate_the_area_of_rhombus.py

---
+++ AREA/calculate_the_area_of_rhombus.py
@@ -0,0 +1,7 @@
+# Calculate area of a rhombus using diagonals
+
+# take input for diagonals
+d1 = float(input("Enter the length of first diagonal: "))
+d2 = float(input("Enter the length of second diagonal: "))
+area = (d1 * d2) / 2
+print("The area of the rhombus is:", area)
