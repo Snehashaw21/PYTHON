191616cb453d206f8ec34c3c5a7c0b36496553c9
Snehashaw21 <snehashaw261@gmail.com>
2025-10-07 17:41:53 +0530

    calculate_area_of_ellipse.py

---
+++ AREA/calculate_area_of_ellipse.py
@@ -0,0 +1,8 @@
+# Calculate area of an ellipse:
+
+import math  
+
+a = float(input("Enter the length of the semi-major axis (a): "))
+b = float(input("Enter the length of the semi-minor axis (b): "))
+area = math.pi * a * b
+print("The area of the ellipse is:", area)
