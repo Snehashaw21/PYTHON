4cafa814bfcd021b41b8e7f95279bfd0c7b7eb74
Snehashaw21 <snehashaw261@gmail.com>
2025-10-09 08:14:38 +0530

    Calculate_area_of_trapezium.py

--- AREA/Calculate_area_of_trapezium.py
+++ AREA/Calculate_area_of_trapezium.py
@@ -3,5 +3,5 @@
 a = float(input("Enter the length of the first parallel side: "))
 b = float(input("Enter the length of the second parallel side: "))
 h = float(input("Enter the height of the trapezium: "))
-area = 0.5 * (a + b) * h
+area = 0.5 * (a + b) * h 
 print("The area of the trapezium is:", area)
