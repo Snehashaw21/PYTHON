21a40ec9c602177aad6cd28dbf9d504127331193
Snehashaw21 <snehashaw261@gmail.com>
2025-10-14 22:58:02 +0530

    ele_greater_then_average_value.py

---
+++ ARRAY/ele_greater_then_average_value.py
@@ -0,0 +1,12 @@
+#Print all elements that are greater than the average value:
+
+def average(arr):
+    avg = sum(arr) / len(arr)
+    print("Average value:", avg)
+    print("Elements greater than average:")
+    for num in arr:
+        if num > avg:
+            print(num, end=" ")
+
+arr = [10, 20, 30, 40, 50]
+average(arr)
