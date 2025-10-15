79d979483ddadc3d039f38a52277450952a31bec
Snehashaw21 <snehashaw261@gmail.com>
2025-10-15 23:10:16 +0530

    print statement for average value calculation

--- ARRAY/ele_greater_then_average_value.py
+++ ARRAY/ele_greater_then_average_value.py
@@ -2,7 +2,7 @@
 
 def average(arr):
     avg = sum(arr) / len(arr)
-    print("Average value:", avg)
+    print("Average value:", avg) 
     print("Elements greater than average:")
     for num in arr:
         if num > avg:
