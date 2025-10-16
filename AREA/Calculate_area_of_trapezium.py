a2dfb45aba4183f24945783629a17f3aaa46c808
Snehashaw21 <snehashaw261@gmail.com>
2025-10-16 10:33:40 +0530

    print statement formatting in trapezium area calculation

--- AREA/Calculate_area_of_trapezium.py
+++ AREA/Calculate_area_of_trapezium.py
@@ -4,4 +4,4 @@ a = float(input("Enter the length of the first parallel side: "))
 b = float(input("Enter the length of the second parallel side: "))
 h = float(input("Enter the height of the trapezium: "))
 area = 0.5 * (a + b) * h 
-print("The area of the trapezium is:", area)
+print("The area of the trapezium is:", area) 
