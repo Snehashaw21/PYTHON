5c8bd1007ffad1c48171105773e6c7bb9635208f
Snehashaw21 <snehashaw261@gmail.com>
2025-10-25 10:24:53 +0530

    Replace_occurrences_one_element_to_another_element.py

--- ARRAY/Replace_occurrences_one_element_to_another_element.py
+++ ARRAY/Replace_occurrences_one_element_to_another_element.py
@@ -4,7 +4,7 @@ def replace_all(arr, old, new):
     for i in range(len(arr)):
         if arr[i] == old:
             arr[i] = new
-    return arr
+    return arr 
 
 arr = [10, 20, 30, 20, 40, 20, 50]
 print("Original array:", arr)
